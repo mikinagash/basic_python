309b4fdb79c4099e5de55d3f2ab0c8d672344329
miki <mikinagash1997@gmail.com>
2022-03-09 21:29:02 +0200

    finish basic_python Exercises

--- basic_python.py
+++ basic_python.py
@@ -1,9 +1,53 @@
 #1
-# x = 0
-# while x < 20:
-#     if x % 2 == 1:
-#         print(x, "not even number  ")
-#     x = x + 1
+# number = int(input("enter number : \n"))
+# print(number**2)
+
+#2
+# age = int(input("what is your age ? :"))
+# print(age + 20)
+
+#3
+# price = int(input("what is the price ? : "))
+# print(price *1.17)
+
+#4
+# num1 = int(input("enter number:"))
+# num2 = int(input("enter number :"))
+# print(num1+num2,num1-num2,num1*num2)
+
+#5
+# print("welcome\n 3/9/20 רביעי")
+
+#6
+# num1 = float(input("enter number :"))
+# num2 = float(input("enter number :"))
+# num3 = float(input("enter number :"))
+# num4 = float(input("enter number :"))
+# print(num1 + num2 + num3 + num4 / 4)
+
+#7
+# nails = int(input("how many nails do you have ? \n"))
+# if nails <= 40:
+#     print("you need only one box !")
+# elif nails > 40 and nails < 80:
+#     print("you need two boxes !")
+# elif nails > 80 and nails < 120:
+#     print("you three boxes")
+
+#8
+# distance = int(input("how much kilo-meter do you have ? "))
+# speed = int(input("what speed are you ? "))
+# print( "this is your minut and hour - ",distance * speed,  distance / speed)
+
+
+
+#9
+#No .Python does not store two values in one variable
+
+#10
+# x = str(0)
+# number = int(input("what is your phone number :"))
+# print("THE NUMBER IS : ", x+str(number))
 
 
 
