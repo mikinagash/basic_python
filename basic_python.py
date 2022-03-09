2f28c6afa2270fa82f83558ddd5c454ddbd5bd21
miki <mikinagash1997@gmail.com>
2022-03-09 15:12:42 +0200

    asdasd

---
+++ basic_python.py
@@ -0,0 +1,20 @@
+#1
+# x = 0
+# while x < 20:
+#     if x % 2 == 1:
+#         print(x, "not even number  ")
+#     x = x + 1
+
+#2
+# num = 20
+# for num in range(0, num+1):
+#     if num % 2 == 1:
+#         print(num, "number not even ")
+
+#3.1
+# x = int(input("enter number:"))
+# while x < 20:
+#      if x % 2 == 1:
+#          print(x, "not even number  ")
+#      x = x +1
+
